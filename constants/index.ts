87dd35b2f06561a8f6a3a53749547a8c3e924698
ianemmanuel <emanu231999@gmail.com>
2025-09-06 14:15:18 +0300

    Landing page created

---
+++ constants/index.ts
@@ -0,0 +1,88 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How TravelAssist Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+
+
+export const FEATURES = [
+  {
+    title: "Many new locations every month",
+    icon: "/location.svg",
+    variant: "orange",
+    description:
+      "Plan your trip effortlessly with our AI assistant. Get personalized itineraries, proactive alerts about delays or strikes, and real-time itinerary syncing across flights, hotels, and activities.",
+  },
+  {
+    title: "Smart Visa & Document Vault",
+    icon: "/tech.svg",
+    variant: "blue",
+    description:
+      "Never stress about travel paperwork again. Instantly check visa requirements, upload documents securely, and store your passport, insurance, and ID in an encrypted Travel Vault.",
+  },
+  {
+    title: "Set a travel schedule",
+    icon: "/calendar.svg",
+    variant: "purple",
+    description:
+      "Go beyond tourist traps. Discover hidden gems, influencer-curated city guides, and AR-powered city walks tailored to your passions, from street food to art galleries.",
+  },
+  {
+    title: "Social & Seamless Travel",
+    icon: "/map.svg",
+    variant: "pink",
+    description:
+      "Invite friends to plan trips together, split costs, and vote on activities. Solo travelers can connect with verified travel buddies nearby for safe, memorable experiences.",
+  },
+];
+
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'About TravelAssist',
+      'Press Releases',
+      'Environment',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['Travel Fans', 'Hiking TravelAssist', 'TravelAssist society'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'officer@travelassist.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
